5adff6ca6252c69d2d3b7f3bacabb7ec17fe151f
Sanket Shinde <70633400+ssankey@users.noreply.github.com>
2023-08-08 00:03:56 +0530

    new nav

--- frontend/src/compoents/Navbar/Navbar.js
+++ frontend/src/compoents/Navbar/Navbar.js
@@ -3,13 +3,29 @@ import { FiMenu, FiX, FiChevronDown } from 'react-icons/fi';
 import { navLinks } from '../../assets/data/data';
 import ContactBar from '../Contact/contactBar';
 import { Link } from 'react-router-dom';
-import logo from '../../assets/images/mountain forest.svg'
+import logo from '../../assets/images/mountain forest.svg';
+
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen((prevIsOpen) => !prevIsOpen);
   };
+  const [isLocationDropdownOpen, setIsLocationDropdownOpen] = useState(false);
+  const [isThemesDropdownOpen, setIsThemesDropdownOpen] = useState(false);
+
+  // Toggle function for each dropdown
+  const toggleLocationDropdown = () => {
+    setIsLocationDropdownOpen((prevState) => !prevState);
+
+  };
+
+  const toggleThemesDropdown = () => {
+    setIsThemesDropdownOpen((prevState) => !prevState);
+
+  };
+
+
 
   return (
     <>
@@ -19,7 +35,7 @@ const Navbar = () => {
           <Link to={'/'} >
             <div className="flex items-center space-x-4">
               <div className="text-xl w-16 h-16 rounded-full bg-cover bg-center object-cover font-bold">
-                <img src={logo} className='rounded-full  bg-green-900' alt='logo'/>
+                <img src={logo} className='rounded-full  bg-green-900' alt='logo' />
               </div>
             </div>
           </Link>
@@ -28,10 +44,40 @@ const Navbar = () => {
             <Link to={'/'}>
               <p className=''>Home</p>
             </Link>
-            <NavbarLinks links={navLinks.locations} title="Locations" />
-            <NavbarLinks links={navLinks.Packages} title="Themes" />
+            <div className="relative">
+              <button onClick={toggleLocationDropdown} className="flex items-center space-x-1 focus:outline-none">
+                Locations <FiChevronDown className="ml-2 text-sm" />
+              </button>
+              {isLocationDropdownOpen && (
+                <div className="sm:absolute relative bg-green-600 z-[100000] py-5 w-[230px] pl-5  text-white rounded shadow-lg mt-2">
+                  {navLinks.locations.map((location, index) => (
+                    <Link key={index} to={location.main_link}>
+                      <p className="flex items-center space-x-2">
+                        {location.main_heading}
+                      </p>
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </div>
+            <div className="relative">
+              <button onClick={toggleThemesDropdown} className="flex items-center space-x-1 focus:outline-none">
+                Themes <FiChevronDown className="ml-2 text-sm" />
+              </button>
+              {isThemesDropdownOpen && (
+                <div className="sm:absolute relative bg-green-600 z-[100000] py-5 w-[230px] pl-5  text-white rounded shadow-lg mt-2">
+                  {navLinks.Packages.map((tourPackage, index) => (
+                    <Link key={index} to={tourPackage.package_link}>
+                      <p className="flex items-center space-x-2">
+                        {tourPackage.package_name}
+                      </p>
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </div>
             <Link to={'/services'}>
-              <p className="hover:text-blue-200 ">
+              <p className="hover:text-blue-200">
                 Services
               </p>
             </Link>
@@ -57,8 +103,38 @@ const Navbar = () => {
             <Link to={'/'}>
               <p>Home</p>
             </Link>
-            <NavbarLinks links={navLinks.locations} title="Locations" />
-            <NavbarLinks links={navLinks.Packages} title="Themes" />
+            <div className="relative">
+              <button onClick={toggleLocationDropdown} className="flex items-center space-x-1 focus:outline-none">
+                Locations <FiChevronDown className="ml-2 text-sm" />
+              </button>
+              {isLocationDropdownOpen && (
+                <div className="mt-2 py-2 w-full bg-green-600 rounded-lg shadow-lg">
+                  {navLinks.locations.map((location, index) => (
+                    <Link key={index} to={location.main_link}>
+                      <p className="flex items-center space-x-2">
+                        {location.main_heading}
+                      </p>
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </div>
+            <div className="relative">
+              <button onClick={toggleThemesDropdown} className="flex items-center space-x-1 focus:outline-none">
+                Themes <FiChevronDown className="ml-2 text-sm" />
+              </button>
+              {isThemesDropdownOpen && (
+                <div className="mt-2 py-2 w-full bg-green-600 rounded-lg shadow-lg">
+                  {navLinks.Packages.map((tourPackage, index) => (
+                    <Link key={index} to={tourPackage.package_link}>
+                      <p className="flex items-center space-x-2">
+                        {tourPackage.package_name}
+                      </p>
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </div>
             <Link to={'/services'}>
               <p className="hover:text-blue-200">
                 Services
@@ -73,39 +149,6 @@ const Navbar = () => {
         )}
       </nav>
     </>
-  );
-};
-
-const NavbarLinks = ({ title, links }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsOpen((prevIsOpen) => !prevIsOpen);
-  };
-
-  return (
-    <div className="relative">
-      <button
-        onClick={toggleDropdown}
-        className="flex items-center space-x-1 hover:text-green-600 focus:outline-none"
-      >
-        {title}
-        <FiChevronDown className="ml-2 text-sm" />
-      </button>
-      {isOpen && (
-        <div className="sm:absolute relative bg-green-600 z-[100000] py-5 w-[230px] pl-5  text-white rounded shadow-lg mt-2">
-          {links.map((link, index) => (
-            <a
-              key={index}
-              href={link.main_link || link.package_link}
-              className="block text-lg my-2"
-            >
-              {link.main_heading || link.package_name}
-            </a>
-          ))}
-        </div>
-      )}
-    </div>
   );
 };
 
